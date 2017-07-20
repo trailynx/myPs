9aee75a4d4e55c0b1a3b7aec88d6f03c98ca6b29
trailynx <trailynx@outlook.com>
2017-07-20 16:12:16 +0200

    add function to find locking process

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -64,6 +64,30 @@ function GitPushProfile ($Message)
 
 ############ windows related stuff ############
 
+function FindLockingProcessForDLL ($lockedFilePath)
+{
+ Get-Process | foreach{$processVar = $_;$_.Modules | foreach{if($_.FileName -eq $LockedFilePath){$processVar.Name + " PID:" + $processVar.id}}}
+} 
+
+function TestFileLock {
+    ## Attempts to open a file and trap the resulting error if the file is already open/locked
+    param ([string]$filePath )
+    $filelocked = $false
+    $fileInfo = New-Object System.IO.FileInfo $filePath
+    trap {
+        Set-Variable -name filelocked -value $true -scope 1
+        continue
+    }
+    $fileStream = $fileInfo.Open( [System.IO.FileMode]::OpenOrCreate,[System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None )
+    if ($fileStream) {
+        $fileStream.Close()
+    }
+    $obj = New-Object Object
+    $obj | Add-Member Noteproperty FilePath -value $filePath
+    $obj | Add-Member Noteproperty IsLocked -value $filelocked
+    $obj
+} 
+
 function KillProcess ($Name)
 {
 	#gps | where {$_.Name -eq $Name} | kill	
